d6f94abf6ed6804f724c30eeea701d9866c6f7a4
Max Morgan <maxmmorgan93@gmail.com>
2022-11-21 15:13:22 -0600

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            //break
+
+            Console.WriteLine("What is your favorite color");
+            string color = Console.ReadLine();
+
+            //break
+
+            Console.WriteLine("What is your favorite animal");
+            string animal = Console.ReadLine();
+
+            //break
+
+            Console.WriteLine("What is your favorite band");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"{name} dyed his hair {color} because he was going to an {band} show. He took Bob, his {animal} to the show as well");
+
         }
     }
 }
